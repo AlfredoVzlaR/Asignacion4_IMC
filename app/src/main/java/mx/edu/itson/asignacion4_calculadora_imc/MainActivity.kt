1b6661ec48d9678607aed566f44275a0b1920170
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2023-02-02 21:13:28 -0700

    Commit Inicial

---
+++ app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
@@ -0,0 +1,65 @@
+package mx.edu.itson.asignacion4_calculadora_imc
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+   //
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val calcular: Button = findViewById(R.id.btnCalcular)
+        calcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    fun calcular(){
+        var etPeso:EditText=findViewById(R.id.peso);
+        var peso:Float=etPeso.text.toString().toFloat();
+        var etEstatura:EditText=findViewById(R.id.estatura);
+        var estatura:Float=etEstatura.text.toString().toFloat();
+
+        var IMC = peso/(estatura*estatura)
+
+        val tvIMC:TextView=findViewById(R.id.IMC)
+        val tvEstatus:TextView=findViewById(R.id.estatus)
+
+        tvIMC.setText(IMC.toString())
+
+        when{
+            IMC <18.5 -> {
+                tvEstatus.setText("Bajo de peso")
+                tvEstatus.setBackgroundResource(R.color.md_brown_400)
+            }
+            IMC >=18.5 && IMC <= 24.9 ->{
+                tvEstatus.setText("Saludable")
+                tvEstatus.setBackgroundResource(R.color.md_green_500)
+            }
+            IMC > 24.9 && IMC <= 29.9 ->{
+                tvEstatus.setText("Sobrepeso")
+                tvEstatus.setBackgroundResource(R.color.md_amber_100)
+            }
+            IMC >29.9 && IMC <= 34.9 -> {
+                tvEstatus.setText("Obesidad de grado 1")
+                tvEstatus.setBackgroundResource(R.color.md_yellow_400)
+            }
+            IMC > 34.9 && IMC <= 39.9 -> {
+                tvEstatus.setText("Obesidad de grado 2")
+                tvEstatus.setBackgroundResource(R.color.md_orange_300)
+            }
+            IMC >=40 -> {
+                tvEstatus.setText("Obesidad de grado 3")
+                tvEstatus.setBackgroundResource(R.color.md_red_500)
+            }
+        }
+    }
+}
