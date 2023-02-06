dd67dde757e5b67557d45be9732894ae699d49f8
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2023-02-05 20:07:46 -0700

    Actualización

--- app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
@@ -23,43 +23,58 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("ResourceAsColor")
     fun calcular(){
-        var etPeso:EditText=findViewById(R.id.peso);
-        var peso:Float=etPeso.text.toString().toFloat();
-        var etEstatura:EditText=findViewById(R.id.estatura);
-        var estatura:Float=etEstatura.text.toString().toFloat();
 
-        var IMC = peso/(estatura*estatura)
 
-        val tvIMC:TextView=findViewById(R.id.IMC)
-        val tvEstatus:TextView=findViewById(R.id.estatus)
+            var etPeso: EditText = findViewById(R.id.peso);
+            var etEstatura: EditText = findViewById(R.id.estatura)
+            var peso: Double = 0.0
+            var estatura: Double = 0.0
 
-        tvIMC.setText(IMC.toString())
+            val tvIMC: TextView = findViewById(R.id.IMC)
+            val tvEstatus: TextView = findViewById(R.id.estatus)
 
-        when{
-            IMC <18.5 -> {
-                tvEstatus.setText("Bajo de peso")
-                tvEstatus.setBackgroundResource(R.color.md_brown_400)
+            if(etPeso.text.toString().isEmpty()||etEstatura.text.toString().isEmpty()){
+                tvIMC.setText("Debes ingresar datos")
+                return
             }
-            IMC >=18.5 && IMC <= 24.9 ->{
-                tvEstatus.setText("Saludable")
-                tvEstatus.setBackgroundResource(R.color.md_green_500)
+
+            try {
+                peso= etPeso.text.toString().toDouble()
+                estatura = etEstatura.text.toString().toDouble()
+
+            }catch (e: java.lang.Exception){
+                tvIMC.setText("Debes ingresar datos")
             }
-            IMC > 24.9 && IMC <= 29.9 ->{
-                tvEstatus.setText("Sobrepeso")
-                tvEstatus.setBackgroundResource(R.color.md_amber_100)
-            }
-            IMC >29.9 && IMC <= 34.9 -> {
-                tvEstatus.setText("Obesidad de grado 1")
-                tvEstatus.setBackgroundResource(R.color.md_yellow_400)
-            }
-            IMC > 34.9 && IMC <= 39.9 -> {
-                tvEstatus.setText("Obesidad de grado 2")
-                tvEstatus.setBackgroundResource(R.color.md_orange_300)
-            }
-            IMC >=40 -> {
-                tvEstatus.setText("Obesidad de grado 3")
-                tvEstatus.setBackgroundResource(R.color.md_red_500)
+
+            var IMC = peso / (estatura * estatura)
+
+            tvIMC.setText(IMC.toString())
+
+            when {
+                IMC < 18.5 -> {
+                    tvEstatus.setText("Bajo de peso")
+                    tvEstatus.setBackgroundResource(R.color.md_brown_400)
+                }
+                IMC >= 18.5 && IMC <= 24.9 -> {
+                    tvEstatus.setText("Saludable")
+                    tvEstatus.setBackgroundResource(R.color.md_green_500)
+                }
+                IMC > 24.9 && IMC <= 29.9 -> {
+                    tvEstatus.setText("Sobrepeso")
+                    tvEstatus.setBackgroundResource(R.color.md_amber_100)
+                }
+                IMC > 29.9 && IMC <= 34.9 -> {
+                    tvEstatus.setText("Obesidad de grado 1")
+                    tvEstatus.setBackgroundResource(R.color.md_yellow_400)
+                }
+                IMC > 34.9 && IMC <= 39.9 -> {
+                    tvEstatus.setText("Obesidad de grado 2")
+                    tvEstatus.setBackgroundResource(R.color.md_orange_300)
+                }
+                IMC >= 40 -> {
+                    tvEstatus.setText("Obesidad de grado 3")
+                    tvEstatus.setBackgroundResource(R.color.md_red_500)
+                }
             }
         }
     }
-}
